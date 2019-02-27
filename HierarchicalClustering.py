b34d8449eb3c2c5ae4a1539fd8585912d333f42c
SomeshKashyap <47707713+SomeshKashyap@users.noreply.github.com>
2019-02-28 01:03:42 +0530

    Add files via upload

---
+++ HierarchicalClustering.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 22 19:44:44 2018
+
+@author: SOMESH KASHYAP
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+dataset = pd.read_csv('Mall_Customers.csv')
+X = dataset.iloc[: , [3,4]].values
+
+
+import scipy.cluster.hierarchy as sch
+dendrogram = sch.dendrogram(sch.linkage(X ,method ='ward'))
+plt.title('Dendrogram')
+plt.xlabel('Customers')
+plt.ylabel('Euclidean Distance')
+plt.show()
+
+from sklearn.cluster import AgglomerativeClustering
+hc = AgglomerativeClustering(n_clusters =5 , affinity ='euclidean' ,linkage ='ward')
+y_hc = hc.fit_predict(X)
+
+plt.scatter(X[y_hc==0 ,0] ,X[y_hc==0 ,1] , s=100 ,c='red' , label = 'careful')
+plt.scatter(X[y_hc==1 ,0] ,X[y_hc==1 ,1] , s=100 ,c='blue' , label = 'standard')
+plt.scatter(X[y_hc==2 ,0] ,X[y_hc==2 ,1] , s=100 ,c='green' , label = 'Target')
+plt.scatter(X[y_hc==3 ,0] ,X[y_hc==3 ,1] , s=100 ,c='cyan' , label = 'careless')
+plt.scatter(X[y_hc==4 ,0] ,X[y_hc==4 ,1] , s=100 ,c='magenta' , label = 'Sensible')
+plt.title('Clusters of clients')
+plt.xlabel('Annual income')
+plt.ylabel('Spending Score')
+plt.legend()
+plt.show()
